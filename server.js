75dadf86a2a619d0e7772661538cabfd0bed1813
flaviomotamedeiros <102139269+flaviomotamedeiros@users.noreply.github.com>
2026-05-12 15:28:38 -0300

    Initialize Express server in server.js
    Set up Express server with basic configuration.

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+// TODO: implemente a rota GET /
+// A resposta deve conter a palavra "Hello"
+
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000');
+});
